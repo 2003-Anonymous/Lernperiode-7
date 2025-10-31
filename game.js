12c89f7ad2a87df666229b5e8115b0f96dbe62a4
2003-Anonymous <J.Sommerhalder.inf24@stud.bbbaden.ch>
2025-10-31 12:19:50 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([47.3769, 8.5417], 13);
+var map = L.map('map').setView([47.29977627719577, 8.12394213320905], 13);
 
         
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -6,16 +6,21 @@ var map = L.map('map').setView([47.3769, 8.5417], 13);
         }).addTo(map);
 
         
-        L.marker([47.3769, 8.5417]).addTo(map)
-            .bindPopup("Zürich")
-            .openPopup();
+        // L.marker([47.29977627719577, 8.12394213320905]).addTo(map)
+        //     .bindPopup("Oberkulm")
+        //     .openPopup();
 
 const missiles = [
-    { name: "RS-28 Sarmat"},
+    { name: "RS-28 Sarmat",
+      src: "Models/fattah_hypersonic_missile.glb",
+      radius: 50
+    },
     { name: "RSM-56 Bulava"},
     { name: "RS-24 Yars"},
     { name: "R-29RMU Sineva"}
 ]
+
+const viewer = document.getElementById("modelViewer");
 
 function createList(missiles){
     const missileList = document.getElementById("missileList");
@@ -24,18 +29,90 @@ function createList(missiles){
         const newLi = document.createElement("li");
         newLi.textContent = element.name;
         newLi.addEventListener('click', () =>{
-            selectedCard = element;
-            const cardName = document.getElementById("saTop");
-            const cardPrice = document.getElementById("cardPrice");
-            
-            cardName.textContent = element.name;
-            cardPrice.textContent = element.price.toFixed(2) + " CHF";
 
-            const amount = parseInt(amountInput.value) || 0;
-            const total = amount * selectedCard.price;
-            document.getElementById("totalPrice").textContent = total.toFixed(2) + " CHF";
 
+            viewer.src = element.src;
         });
-        cardList.appendChild(newLi);
+        missileList.appendChild(newLi);
     });  
 }
+
+
+L.circle([47.3769, 8.5417], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.3,
+    radius: 2500
+}).addTo(map).bindPopup("25 km Umkreis von Zürich");
+
+
+var BaseIcon = L.icon({
+    iconUrl: 'Images/barracks.png',    
+    iconSize: [20, 20],       
+    iconAnchor: [10, 10],     
+    popupAnchor: [0, -40]     
+});
+
+
+L.marker([47.3769, 8.5417], { icon: BaseIcon })
+    .addTo(map)
+    .bindPopup("Base");
+
+
+
+
+
+// Wenn du auf die Karte klickst, füge einen Marker hinzu
+map.on('click', function(e) {
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("Marker bei: " + e.latlng.toString());
+});
+
+let tempMarker = "";
+map.on('contextmenu', function(e){
+    const menu = document.getElementById("contextMenu");
+    menu.style.display = 'block';
+
+    
+    //Mauskoordinaten, wird nicht vom Zoom beeinflusst
+    menu.style.left = e.originalEvent.clientX + 5 + 'px';
+    menu.style.top = e.originalEvent.clientY + 5 + 'px';
+
+    menu.dataset.lat = e.latlng.lat;
+    menu.dataset.lng = e.latlng.lng;
+
+    tempMarker = L.marker(e.latlng).addTo(map);
+});
+
+document.querySelectorAll('#contextMenu .menu-item').forEach(item => {
+    item.addEventListener('click', () => {
+        const menu = document.getElementById('contextMenu');
+        const lat = parseFloat(menu.dataset.lat);
+        const lng = parseFloat(menu.dataset.lng);
+
+        const type = item.dataset.type;
+
+        let icon;
+        if(type === 'base'){
+            icon = L.icon({iconUrl: 'Images/barracks.png', iconSize:[20,20], iconAnchor:[10,10]});
+        }
+        else if(type === 'camp')
+        {
+            icon = L.icon({iconUrl: 'Images/target.png', iconSize:[20,20], iconAnchor:[10,10]});
+        }
+
+        L.marker([lat, lng], {icon: icon}).addTo(map);
+
+        map.removeLayer(tempMarker);
+        menu.style.display = 'none';
+    })
+})
+
+
+
+
+
+
+
+
+createList(missiles);
