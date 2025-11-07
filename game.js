4ccfd5efff70c66f573e8b6c6384eccbf3143ebf
2003-Anonymous <J.Sommerhalder.inf24@stud.bbbaden.ch>
2025-11-07 12:09:28 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([47.29977627719577, 8.12394213320905], 13);
+var map = L.map('map').setView([47.3769, 8.5417], 13);
 
         
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -11,40 +11,43 @@ var map = L.map('map').setView([47.29977627719577, 8.12394213320905], 13);
         //     .openPopup();
 
 const missiles = [
-    { name: "RS-28 Sarmat",
-      src: "Models/fattah_hypersonic_missile.glb",
-      radius: 50
+    { 
+        name: "ATACMS (USA)",
+        type: "cruise",
+        warhead: 230,
+        radius: 300,
+        range: 300000,
+        src: "Models/fattah-1.glb"
     },
-    { name: "RSM-56 Bulava"},
-    { name: "RS-24 Yars"},
-    { name: "R-29RMU Sineva"}
+    { 
+        name: "SCUD-B (UdSSR)",
+        type: "ballistic",
+        warhead: 985,
+        radius: 750,
+        range: 300000
+    },
+    { 
+        name: "Iskander-M (Russland)",
+        type: "tactical ballistic",
+        warhead: 600,
+        radius: 450,
+        range: 500000
+    },
+    { 
+        name: "Tomahawk (USA)",
+        type: "cruise missile",
+        warhead: 450,
+        radius: 400,
+        range: 1600000
+    },
+    {
+        name: "DF-21 (China)",
+        type: "middlerange ballistic",
+        warhead: 600,
+        radius: 1500,
+        range: 1500000
+    }
 ]
-
-const viewer = document.getElementById("modelViewer");
-
-function createList(missiles){
-    const missileList = document.getElementById("missileList");
-    missileList.innerHTML = "";
-    missiles.forEach ((element) => {
-        const newLi = document.createElement("li");
-        newLi.textContent = element.name;
-        newLi.addEventListener('click', () =>{
-
-
-            viewer.src = element.src;
-        });
-        missileList.appendChild(newLi);
-    });  
-}
-
-
-L.circle([47.3769, 8.5417], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.3,
-    radius: 2500
-}).addTo(map).bindPopup("25 km Umkreis von Zürich");
-
 
 var BaseIcon = L.icon({
     iconUrl: 'Images/barracks.png',    
@@ -54,9 +57,89 @@ var BaseIcon = L.icon({
 });
 
 
-L.marker([47.3769, 8.5417], { icon: BaseIcon })
+let base = L.marker([47.3769, 8.5417], { icon: BaseIcon })
     .addTo(map)
     .bindPopup("Base");
+
+let selectedMissile;
+const viewer = document.getElementById("modelViewer");
+let previewRadius;
+let rangeCircle;
+let selectedElement;
+let previewCenter;
+let target;
+
+function createList(missiles){
+    const missileList = document.getElementById("missileList");
+    missileList.innerHTML = "";
+
+    missiles.forEach ((element) => {
+        const newLi = document.createElement("li");
+        newLi.textContent = element.name;
+        newLi.addEventListener('click', () =>{
+            selectedMissile = element;
+            
+            const name = document.getElementById("name_field");
+            const type = document.getElementById("type_field");
+            const damage = document.getElementById("damage_field");
+            const radius = document.getElementById("radius_field");
+            const range = document.getElementById("range_field");
+
+            name.textContent = element.name;
+            type.textContent = element.type;
+            damage.textContent = element.warhead;
+            radius.textContent = element.radius;
+            range.textContent = element.range;
+
+            viewer.src = element.src;
+
+            if(previewRadius){
+                map.removeLayer(previewRadius);
+                previewRadius = null;
+            }
+            previewRadius = L.circle([0, 0], {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.3,
+                radius: selectedMissile.radius,
+                weight: 1
+            }).addTo(map);
+
+            if(previewCenter){
+                map.removeLayer(previewCenter);
+                previewCenter = null;
+            }
+            previewCenter = L.circleMarker([0, 0],{
+                color: 'red',
+                fillColor: '#f03',
+                radius: 2,
+            }).addTo(map);
+
+            if(rangeCircle){
+                map.removeLayer(rangeCircle);
+                rangeCircle = null;
+            }
+            rangeCircle = L.circle(base.getLatLng(), {
+                color: 'green',
+                fillColor: '#0fa11b',
+                fillOpacity: 0.3,
+                radius: selectedMissile.range
+            }).addTo(map);
+        });
+        missileList.appendChild(newLi);
+    });  
+}
+
+
+// L.circle([47.3769, 8.5417], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.3,
+//     radius: 2500
+// }).addTo(map).bindPopup("25 km Umkreis von Zürich");
+
+
+
 
 
 
@@ -64,9 +147,41 @@ L.marker([47.3769, 8.5417], { icon: BaseIcon })
 
 // Wenn du auf die Karte klickst, füge einen Marker hinzu
 map.on('click', function(e) {
-    L.marker(e.latlng).addTo(map)
-        .bindPopup("Marker bei: " + e.latlng.toString());
+    let distance = map.distance(base.getLatLng(), e.latlng);
+    if(distance < selectedMissile.range){
+
+        if(previewRadius){
+            let radius = L.circle(e.latlng, {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.3,
+                radius: selectedMissile.radius,
+                weight: 1
+            }).addTo(map);
+            target = radius;
+        }
+    
+        if(previewCenter){
+            L.circle(e.latlng,{
+                color: 'red',
+                fillColor: '#f03',
+                radius: 4,
+                weight: 1
+            }).addTo(map);
+        }
+
+        map.removeLayer(previewCenter);
+        previewCenter = null;
+        map.removeLayer(previewRadius);
+        previewRadius = null;
+    }
 });
+
+
+
+
+
+
 
 let tempMarker = "";
 map.on('contextmenu', function(e){
@@ -109,10 +224,70 @@ document.querySelectorAll('#contextMenu .menu-item').forEach(item => {
 })
 
 
+map.on('mousemove', (e) => {
+    if(previewRadius){
+        previewRadius.setStyle({opacity: 1, fillOpacity: 0.3});
+        previewRadius.setLatLng(e.latlng);
+    }
+    if(previewCenter){
+        previewCenter.setStyle({opacity: 1, fillOpacity: 0.3});
+        previewCenter.setLatLng(e.latlng);
+    }
+});
+map.on('mouseout', () => {
+    if(previewRadius){
+        previewRadius.setStyle({opacity: 0, fillOpacity: 0});
+    }
+    if(previewCenter){
+        previewCenter.setStyle({opacity: 0, fillOpacity: 0});
+    }
+});
 
+//lineare interpolation
+function lerp(a, b, t){ return a + (b - a) * t; }
+function launchMissile(start, target, durationSec = 3){
+    const startLatLng = start.getLatLng();
+    const targetLatLng = target.getLatLng();
 
+    let missileIcon = L.icon({iconUrl: 'Images/missile.png', iconSize:[20,20], iconAnchor:[10,10]});
+    const missile = L.marker(startLatLng, { icon: missileIcon }).addTo(map);
 
+    const startTime = performance.now();
+    const durationMs = durationSec * 1000;
 
+    function step(now){
+        const elapsed = now - startTime;
 
+        //gibt einen Wert zwischen 0 und 1 zurück: 1 = am Ziel.
+        const t = Math.min(1, elapsed / durationMs);
+
+        const lat = lerp(startLatLng.lat, targetLatLng.lat, t);
+        const lng = lerp(startLatLng.lng, targetLatLng.lng, t);
+        missile.setLatLng([lat, lng]);
+
+        if(t < 1){
+            requestAnimationFrame(step);
+        } else {
+            missile.remove();
+            target.setStyle({ fillColor: 'black', color: 'black'});
+        }
+    }
+    requestAnimationFrame(step);
+}
+
+const launchBtn = document.getElementById("launchBtn");
+launchBtn.addEventListener('click', () =>{
+    if (!target) {
+        alert("Bitte zuerst ein Ziel auswählen!");
+        return;
+    }
+    if(target){
+        launchMissile(base, target, 3);
+    }
+    
+})
 
 createList(missiles);
+
+
+//<a href="https://www.flaticon.com/free-icons/missile" title="missile icons">Missile icons created by Nhor Phai - Flaticon</a>
